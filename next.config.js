5b33d5d99272a1abdb41626369a5a1abc447ba3b
Md Irshad <mdirshaddev@gmail.com>
2022-03-26 22:14:59 +0530

    feat(nextjs,config): 🛠️ Updated Next.js Configuration
    * authToken is added within Sentry Webpack plugin options

--- next.config.js
+++ next.config.js
@@ -23,8 +23,8 @@ const sentryWebpackPluginOptions = {
   // recommended:
   //   release, url, org, project, authToken, configFile, stripPrefix,
   //   urlPrefix, include, ignore
-
-  silent: true // Suppresses all logs
+  silent: true, // Suppresses all logs
+  authToken: process.env.NEXT_PUBLIC_SENTRY_AUTH_TOKEN
   // For all available options, see:
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
