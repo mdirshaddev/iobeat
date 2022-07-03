7bcd3af028642cb06d5b90cc6c351194ead7f46c
mdirshaddev <mdirshaddev@gmail.com>
2022-07-03 11:51:57 +0530

    feat: 🧑‍💻 Nextjs authentication with Google and GitHub provider
    * Google Auth provider
    * GitHub Auth provider
    * Session using jwt and max age for 30 days

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,25 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import GitHubProvider from 'next-auth/providers/github';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { prisma } from 'libs/prisma';
+
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET
+    }),
+    GitHubProvider({
+      clientId: process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_CLIENT_SECRET
+    })
+  ],
+  session: {
+    strategy: 'jwt',
+
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 30 * 24 * 60 * 60 // 30 days
+  }
+});
