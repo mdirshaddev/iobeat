1fe896d26d5ab99e9389a9c9c17aff8c53daa259
mdirshaddev <mdirshaddev@gmail.com>
2022-07-12 13:17:38 +0530

    fix(ci): 🔧 Cypress Video Upload Issue
    * Change in Cypress Story
    * Change in Cypress configuration
    * Change in GitHub actions for Cypress Step

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   projectId: 'o3nt18',
   screenshotOnRunFailure: true,
+  screenshotsFolder: 'cypress/screenshots',
   scrollBehavior: 'bottom',
   video: true,
+  videosFolder: 'cypress/videos',
+  videoCompression: 15,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/landing.cy.ts
+++ cypress/e2e/landing.cy.ts
@@ -1,6 +1,7 @@
 describe('Our Iobeat Web Application', () => {
   it('passes', () => {
     cy.visit('http://localhost:3000');
+    cy.screenshot();
   });
 });
 
