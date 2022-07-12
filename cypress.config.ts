99290bdc19b5a64a80a447e15de0ede97f77c6f6
mdirshaddev <mdirshaddev@gmail.com>
2022-07-12 11:41:29 +0530

    feat: 🏗️ Huge Architecture changes for actions, .gitignore and cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: 'o3nt18',
+  screenshotOnRunFailure: true,
+  scrollBehavior: 'bottom',
+  video: true,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
